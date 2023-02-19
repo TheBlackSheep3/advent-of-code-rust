f6bb2aabc88aaaed1de3309e1124f7dd91c09795
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2023-02-19 10:21:49 +0100

    added function to modify file and directory collection of directory

--- src/year_2022/day07.rs
+++ src/year_2022/day07.rs
@@ -14,6 +14,22 @@ impl<'a> Directory<'a> {
         }
         Some(sum)
     }
+
+    fn add_dir(&mut self, dir: &'a Directory) -> () {
+        self.directories.push(dir);
+    }
+
+    fn clear_dirs(&mut self) -> () {
+        self.directories.clear();
+    }
+
+    fn add_file(&mut self, file: &'a File) -> () {
+        self.files.push(file);
+    }
+
+    fn clear_files(&mut self) -> () {
+        self.files.clear();
+    }
 }
 
 struct File {
@@ -51,26 +67,26 @@ $ ls
     fn get_size() {
         let mut root = Directory{files: Vec::new(), directories: Vec::new()};
         let f = File { size: usize::MAX};
-        root.files.push(&f);
+        root.add_file(&f);
         assert_eq!(root.get_size(), Some(usize::MAX));
-        root.files.push(&f);
+        root.add_file(&f);
         assert_eq!(root.get_size(), None);
-        root.files.clear();
+        root.clear_files();
         assert_eq!(root.get_size(), Some(0));
         let mut dir1 = Directory{ files:Vec::new(), directories:Vec::new()};
         let f = File { size: 297 };
-        dir1.files.push(&f);
+        dir1.add_file(&f);
         let f = File { size: 92 };
-        dir1.files.push(&f);
+        dir1.add_file(&f);
         let mut dir2 = Directory{ files:Vec::new(), directories:Vec::new()};
         assert_eq!(dir1.get_size(), Some(389));
         let f = File { size: 201 };
-        dir2.files.push(&f);
+        dir2.add_file(&f);
         let f = File { size: 927 };
-        dir2.files.push(&f);
+        dir2.add_file(&f);
         assert_eq!(dir2.get_size(), Some(1128));
-        dir1.directories.push(&dir2);
-        root.directories.push(&dir1);
+        dir1.add_dir(&dir2);
+        root.add_dir(&dir1);
         assert_eq!(dir1.get_size(), Some(1517));
         assert_eq!(root.get_size(), Some(1517));
     }
