e6f7bb19604802a5ebd0ebefb5ef69f577be41e1
Niklas Dießner <40034835+TheBlackSheep3@users.noreply.github.com>
2024-12-01 13:33:04 +0100

    year_2023 day 1

--- src/year_2023/day01.rs
+++ src/year_2023/day01.rs
@@ -1,25 +1,85 @@
-use std::num::ParseIntError;
 use regex::Regex;
+use std::num::ParseIntError;
 
-pub fn get_calibration_values(calibration_input: &str) -> Result<Vec<i32>, ParseIntError> {
+fn get_calibration_values_only_digits(calibration_input: &str) -> Result<Vec<i32>, ParseIntError> {
     let lines = calibration_input.lines();
     let re = Regex::new(r"^\D*(?P<first>\d)?.*(?P<second>\d)").unwrap();
-    Ok(lines.map(|l| {
-        let caps = re.captures(l).expect("invalid calibration line");
-        match caps.name("first") {
-            Some(first) => {
-                first.as_str().parse::<i32>().expect("failed to parse first digit") * 10 + caps.name("second").expect("failed to find second digit").as_str().parse::<i32>().expect("failed to parse second digit")
+    Ok(lines
+        .map(|l| {
+            let caps = re.captures(l).expect("invalid calibration line");
+            match caps.name("first") {
+                Some(first) => {
+                    first
+                        .as_str()
+                        .parse::<i32>()
+                        .expect("failed to parse first digit")
+                        * 10
+                        + caps
+                            .name("second")
+                            .expect("failed to find second digit")
+                            .as_str()
+                            .parse::<i32>()
+                            .expect("failed to parse second digit")
+                }
+                None => {
+                    let digit = caps
+                        .name("second")
+                        .expect("failed to find single digit")
+                        .as_str()
+                        .parse::<i32>()
+                        .expect("failed to parse single digit");
+                    digit * 11
+                }
             }
-            None => {
-                let digit = caps.name("second").expect("failed to find single digit").as_str().parse::<i32>().expect("failed to parse single digit");
-                digit * 11
-            }
-        }
-    }).collect())
+        })
+        .collect())
 }
 
-pub fn get_calibration_values_sum(calibration_input: &str) -> Result<i32, ParseIntError> {
-    get_calibration_values(calibration_input).map(|v| v.iter().sum())
+pub fn get_calibration_values_sum_only_digits(
+    calibration_input: &str,
+) -> Result<i32, ParseIntError> {
+    get_calibration_values_only_digits(calibration_input).map(|v| v.iter().sum())
+}
+
+pub fn get_calibration_values_sum_with_words(
+    calibration_input: &str,
+) -> Result<i32, ParseIntError> {
+    get_calibration_values_with_words(calibration_input).map(|v| v.iter().sum())
+}
+
+fn get_calibration_values_with_words(calibration_input: &str) -> Result<Vec<i32>, ParseIntError> {
+    let lines = calibration_input.lines();
+    let re = Regex::new(r"^\D*(?P<first>\d)?.*(?P<second>\d)").unwrap();
+    Ok(lines
+        .map(|l| {
+
+            let caps = re.captures(l).expect("invalid calibration line");
+            match caps.name("first") {
+                Some(first) => {
+                    first
+                        .as_str()
+                        .parse::<i32>()
+                        .expect("failed to parse first digit")
+                        * 10
+                        + caps
+                            .name("second")
+                            .expect("failed to find second digit")
+                            .as_str()
+                            .parse::<i32>()
+                            .expect("failed to parse second digit")
+                }
+                None => {
+                    let digit = caps
+                        .name("second")
+                        .expect("failed to find single digit")
+                        .as_str()
+                        .parse::<i32>()
+                        .expect("failed to parse single digit");
+                    digit * 11
+                }
+            }
+        })
+        .collect())
 }
 
 #[cfg(test)]
@@ -31,13 +91,43 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet";
 
+    const TEST_STR_WORDS: &str = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
     #[test]
-    fn get_sum() {
-        assert_eq!(get_calibration_values_sum(TEST_STR).unwrap(), 142);
+    fn get_sum_only_digits() {
+        assert_eq!(
+            get_calibration_values_sum_only_digits(TEST_STR).unwrap(),
+            142
+        );
     }
 
     #[test]
-    fn get_values() {
-        assert_eq!(get_calibration_values(TEST_STR).unwrap(), vec![12, 38, 15, 77]);
+    fn get_sum_with_word() {
+        assert_eq!(
+            get_calibration_values_sum_with_words(TEST_STR_WORDS).unwrap(),
+            281
+        );
+    }
+
+    #[test]
+    fn get_values_only_digits() {
+        assert_eq!(
+            get_calibration_values_only_digits(TEST_STR).unwrap(),
+            vec![12, 38, 15, 77]
+        );
+    }
+
+    #[test]
+    fn get_values_with_words() {
+        assert_eq!(
+            get_calibration_values_with_words(TEST_STR_WORDS).unwrap(),
+            vec![29, 83, 13, 24, 42, 14, 76]
+        );
     }
 }
--- src/year_2023/mod.rs
+++ src/year_2023/mod.rs
@@ -9,13 +9,17 @@ pub fn get_implemented() -> Vec<&'static str> {
 pub fn solve(day: u8, input: &str) {
     match day {
         1 => {
-            match day01::get_calibration_values_sum(input) {
+            match day01::get_calibration_values_sum_only_digits(input) {
                 Ok(x) => println!("part 1: {}", x),
+                Err(e) => println!("{}", e),
+            }
+            match day01::get_calibration_values_sum_with_words(input) {
+                Ok(x) => println!("part 2: {}", x),
                 Err(e) => println!("{}", e),
             }
         }
         2..=25 => {
-            super::print_not_implemented(2022, day);
+            super::print_not_implemented(2023, day);
         }
         _ => println!("{} is not a valid day for challenges", day),
     }
